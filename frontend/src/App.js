2a8a40f71a1f09c2b48af3db465daf6290637c52
jesse <jessechua131@gmail.com>
2020-03-16 13:42:28 -0400

    Finished integrating old js and html scripts into react. Need to test full response request cycle with new React components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,67 @@
-import React from "react";
+import React, {Component} from "react";
 import "./App.css";
 
-import TextInput from './TextInput'
+import Audio from './Audio';
+import TextInput from './TextInput';
 
-function App() {
-  return (
-    <div className="App">
-      <TextInput />
-      <audio controls id="audio-controls">
-        <source id="alda-audio" src="" type="audio/mpeg" />
-        Your browser does not support the audio element.
-      </audio>
-    </div>
-  );
+class App extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      filePath: ""
+    }
+  }
+
+  /**
+  * Takes an Alda formatted string and sends it to server to be 
+  * converted into a mp3 file for playback.
+  * @param {string} aldaCode - An Alda formatted string.
+  * @returns {object} response - A response object from the server with
+  * either the error that occurred or a string representing the file path for the mp3 file.
+  */
+  postAudio = async (aldaCode) => {
+    const response = await fetch('/alda', 
+                                    {
+                                        method: 'post',
+                                        headers: {
+                                        'Accept': 'application/json',
+                                        'Content-Type': 'application/json'
+                                    },
+                                    body: JSON.stringify({'data': aldaCode})
+                                });
+    if (response.status !== 200) {
+      throw Error(response.statusText);
+    }
+
+    const data = await response.json();
+    if (data['status'] !== 200) {
+      throw Error(data['data']);
+    }
+
+    return data;
+  }
+
+  handleClick = async (input) => {
+    try {
+      const data = await this.postAudio(input);
+      this.setState = {
+        filePath: data['data']
+      };
+
+    } catch(error) {
+        console.log("Error processing request: " + error.toString());
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <TextInput handleClick={this.handleClick}/>
+        <Audio filePath={this.state.filePath}/>
+      </div>
+    );
+  }
 }
 
 export default App;
--- frontend/src/TextInput.js
+++ frontend/src/TextInput.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Button from './Button';
 
 class TextInput extends React.Component {
   constructor(props) {
@@ -15,21 +14,22 @@ class TextInput extends React.Component {
     });
   };
 
-  handleClick = () => {
-      //fetch(with this.state.input) 
-  };
-
   render() {
     return (
-      <form>
-        <div class="form-group">
-          <label for="alda-input">Alda Input</label>
-          <textarea class="form-control" id="alda-input" onChange={this.handleChange} rows="3"></textarea>
-        </div>
-        <button id="alda-input-button" type="button" onClick={this.handleClick} class="btn btn-primary">
-          Run Code
-        </button>
-      </form>
+      <div>
+        <form>
+          <div class="form-group">
+            <label for="alda-input">Alda Input</label>
+            <textarea class="form-control" id="alda-input" onChange={this.handleChange} rows="3"></textarea>
+          </div>
+          <button id="alda-input-button" type="button" onClick={this.props.handleClick(this.state.input)} class="btn btn-primary">
+            Run Code
+          </button>
+        </form>
+      </div>
     );
   }
 }
+
+export default TextInput;
+
